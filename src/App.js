c2d860c5000aed9f0a1eefba1efd2f477a15bb33
fish <1742350894@qq.com>
2020-12-17 15:03:05 +0800

    底部tabBar显示

--- src/App.js
+++ src/App.js
@@ -9,15 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <HashRouter>
-          <ul>
-            <li>
-              <Link to="/home">首页</Link>
-            </li>
-            <li>
-              <Link to="/citylist">城市列表</Link>
-            </li>
-          </ul>
-          <Route path="/home" component={Home}></Route>
+          <Route path="/" component={Home}></Route>
           <Route path="/citylist" component={Citylist}></Route>
         </HashRouter>
       </div>
