bee46875be7abe2bc820c6ca733fc6b39aa31c6c
fish <1742350894@qq.com>
2020-12-17 11:51:59 +0800

    '初体验，按钮的实现'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
 //项目根组件
+import React, { Component } from "react";
+
+import { Button } from "antd-mobile";
+// import { Button, WhiteSpace, WingBlank } from "antd-mobile";
+
 export default class App extends Component {
   render() {
-    return <div>首页</div>;
+    return (
+      <div>
+        首页
+        <Button type="primary">primary</Button>
+      </div>
+    );
   }
 }
